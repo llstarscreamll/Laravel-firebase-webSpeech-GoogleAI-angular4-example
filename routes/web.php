0e3f7d6f45912387adf8f63c6d599004704dc579
Johan Alvarez <llstarscreamll@hotmail.com>
2017-09-20 19:02:12 -0500

    remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -13,37 +13,3 @@ use App\Services\FirebaseService;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function (Request $request) {
-	// check if there are not token data on session or
-	// request, redirect to error page
-	if (! $request->has('token') && ! session()->has('token')) {
-		$error = 'Tienes que iniciar sesión.';
-		return redirect('/error?error='.bin2hex($error));
-	}
-
-	// if request has token data, put it on session and clean
-	// the token from the url
-	if ($request->has('token')) {
-		$token = $request->get('token');
-		session()->put('token', $token);
-		return redirect('/');
-	}
-
-	// if session hasn't token, then go to error page, session expired
-	if (! session()->has('token')) {
-		$error = 'Sesión expirada.';
-		return redirect('/error?error='.bin2hex($error));
-	}
-
-	// all ok here, render de Angular app
-	return view('angular-app');
-});
-
-Route::get('/error', function (Request $request) {
-	$error = $request->has('error')
-		? hex2Bin($request->get('error'))
-		: 'Error desconocido';
-
-    return view('welcome', ['error' => $error]);
-});
